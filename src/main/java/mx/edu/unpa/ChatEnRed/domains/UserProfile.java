113f4e4fce22f0ed3a3567d30d888e5240b9f276
I4JI <hernandezaguirrej204@gmail.com>
2025-11-09 17:53:40 -0600

    feat: crea UserProfile.java

---
+++ src/main/java/mx/edu/unpa/ChatEnRed/domains/UserProfile.java
@@ -0,0 +1,33 @@
+package mx.edu.unpa.ChatEnRed.domains;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user_profiles")
+public class UserProfile implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
