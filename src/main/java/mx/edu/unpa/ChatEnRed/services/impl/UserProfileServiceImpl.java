dd030bdf16405ae80dbdf94205563ce6c5f507a9
I4JI <hernandezaguirrej204@gmail.com>
2025-11-09 18:15:06 -0600

    feat: crea UserProfileServiceImpl.java

---
+++ src/main/java/mx/edu/unpa/ChatEnRed/services/impl/UserProfileServiceImpl.java
@@ -0,0 +1,37 @@
+package mx.edu.unpa.ChatEnRed.services.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.edu.unpa.ChatEnRed.domains.UserProfile;
+import mx.edu.unpa.ChatEnRed.repositories.UserProfileRepository;
+import mx.edu.unpa.ChatEnRed.services.UserProfileService;
+
+@Service
+public class UserProfileServiceImpl implements UserProfileService {
+
+    @Autowired
+    private UserProfileRepository repo;
+
+    @Override
+    public Iterable<UserProfile> findAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Optional<UserProfile> findById(int userId) {
+        return repo.findById(userId);
+    }
+
+    @Override
+    public UserProfile save(UserProfile profile) {
+        return repo.save(profile);
+    }
+
+    @Override
+    public void deleteById(int userId) {
+        repo.deleteById(userId);
+    }
+}
