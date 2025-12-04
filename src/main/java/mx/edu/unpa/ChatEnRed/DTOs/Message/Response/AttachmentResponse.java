cbe069c340c97d212f70a2ebd2915d7ea36eafea
José Luis <hernandezaguirrej204@gmail.com>
2025-12-03 19:34:37 -0600

    feat (Attachment): Modifica el modulo Attachment

---
+++ src/main/java/mx/edu/unpa/ChatEnRed/DTOs/Message/Response/AttachmentResponse.java
@@ -0,0 +1,17 @@
+package mx.edu.unpa.ChatEnRed.DTOs.Message.Response;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class AttachmentResponse {
+    private Integer id;
+    private Integer messageId;
+    private String filename;
+    private String mimeType;
+    private Integer size;
+    private String storageUrl;
+    private String checksum;
+    private LocalDateTime createdAt;
+}
--- src/main/java/mx/edu/unpa/ChatEnRed/repositories/AttachmentRepository.java
+++ src/main/java/mx/edu/unpa/ChatEnRed/repositories/AttachmentRepository.java
@@ -1,11 +1,10 @@
 package mx.edu.unpa.ChatEnRed.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import mx.edu.unpa.ChatEnRed.domains.Attachment;
 import org.springframework.stereotype.Repository;
 
-import mx.edu.unpa.ChatEnRed.domains.Attachment;
-
 @Repository
-public interface AttachmentRepository  extends JpaRepository<Attachment, Integer>{
+public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
 
 }
